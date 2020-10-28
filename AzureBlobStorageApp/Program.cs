017638b58735e959f5c2637f062ad9d3d21499aa
dhirajhatwar <70320336+dhirajhatwar@users.noreply.github.com>
2020-10-28 17:09:57 +0000

    BlobStorage app

---
+++ AzureBlobStorageApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using AzureBlobStorageApp.helper;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureBlobStorageApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=dhirajstorageaccount2020;AccountKey=OqZLVZ4OqrmVt8gsFF9OoX+cix8cLpW9Ih3opK3ESGA2GV8O6dTvhiNWiYwj2t7edZ5pppDoY3bbyYFYnJ3lqg==;EndpointSuffix=core.windows.net";
+            storeageHelper helper = new storeageHelper(storageConnectionString);
+           var Iscraeted = await helper.CreateBlobContainer("myfiles");
+            var uri = await helper.UploadBlob(@"C:\Users\student\Desktop\Images\IMG-20200929-WA0003.jpg", "myfiles");
+            Console.WriteLine(uri);
+            Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AzureBlobStorageApp/helper/storeageHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+
+namespace AzureBlobStorageApp.helper
+{
+ public   class storeageHelper
+    {
+        private readonly BlobServiceClient blobServiceClient;
+
+        public storeageHelper(string storageConnectionString)
+        {
+            this.blobServiceClient = new BlobServiceClient(storageConnectionString);
+            
+        }
+
+        public async Task<bool> CreateBlobContainer(string containerName)
+        {
+            try
+            {
+                var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+                await containerClient.CreateIfNotExistsAsync(Azure.Storage.Blobs.Models.PublicAccessType.Blob);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+
+            }
+        }
+
+        public async Task<string> UploadBlob(string filePath, string containerName) {
+
+            var blobName = Path.GetFileName(filePath);
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            var blobClient = containerClient.GetBlobClient(blobName);
+          var response = await   blobClient.UploadAsync(filePath, true);
+            return blobClient.Uri.AbsoluteUri;
+
+        }
+    }
+}
